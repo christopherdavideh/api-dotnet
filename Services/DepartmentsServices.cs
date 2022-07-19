8762a5ef9dd08a0dc5ee9779a7d4bec8d370b852
christopherdavideh <chisk8_28@hotmail.com>
2022-07-19 09:17:12 -0500

    Add Servises to project

---
+++ Services/DepartmentsServices.cs
@@ -0,0 +1,90 @@
+﻿using Dapper;
+using Npgsql;
+using web_api.Models;
+
+namespace web_api.Services
+{
+    public class DepartmentsServices : IDepartmentsServices
+    {
+        CompanyContext _companyContext;
+
+        public DepartmentsServices(CompanyContext companyContext)
+        {
+            _companyContext = companyContext;
+        }
+
+        protected NpgsqlConnection dbConnection()
+        {
+            return new NpgsqlConnection(_companyContext._connection);
+        }
+
+        public async Task<IEnumerable<Departments>> Get()
+        {
+            var dataBase = dbConnection();
+            string sql = "SELECT * FROM departments WHERE status = '1'";
+            return await dataBase.QueryAsync<Departments>(sql, new { });
+        }
+
+        public async Task<bool> Save(Departments department)
+        {
+            var dataBase = dbConnection();
+            string sql = "INSERT INTO departments (created_by, description, name, phone, id_enterprise) VALUES (@Created_by, @Description, @Name, @Phone, @Id_enterprise)";
+            var result = await dataBase.ExecuteAsync(sql, new
+            { 
+                department.Created_by,
+                department.Description,
+                department.Name,
+                department.Phone,
+                department.Id_enterprise
+            });
+
+            return result > 0;
+        }
+
+        public async Task<bool> Update(int id, Departments department)
+        {
+            var dataBase = dbConnection();
+            string sql = @"UPDATE departments SET
+                            modified_by = @Modified_by,
+                            modified_date = @Modified_date,
+                            status = @Status,
+                            description = @Description,
+                            name = @Name,
+                            phone = @Phone,
+                            id_enterprise = @Id_enterprise
+                           WHERE id = @Id ";
+            var result = await dataBase.ExecuteAsync(sql, new 
+            { 
+                department.Modified_by,
+                department.Modified_date,
+                department.Status,
+                department.Description,
+                department.Name,
+                department.Phone,
+                department.Id_enterprise,
+                Id = id
+            });
+
+            return result > 0;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var dataBase = dbConnection();
+            string sql = "UPDATE departments SET status = false WHERE id = @Id ";
+            var result = await dataBase.ExecuteAsync(sql, new { Id = id });
+            return result > 0;
+        }
+
+    }
+}
+
+public interface IDepartmentsServices
+{
+    Task<IEnumerable<Departments>> Get();
+    Task<bool> Save(Departments department);
+
+    Task<bool> Update(int id, Departments department);
+
+    Task<bool> Delete(int id);
+}
---
+++ Services/EmployeesServices.cs
@@ -0,0 +1,123 @@
+﻿using Dapper;
+using Npgsql;
+using web_api.Models;
+
+namespace web_api.Services
+{
+    public class EmployeesServices : IEmployeesServices
+    {
+        CompanyContext _companyContext;
+
+        public EmployeesServices(CompanyContext companyContext)
+        {
+            _companyContext = companyContext;
+        }
+
+        protected NpgsqlConnection dbConnection()
+        {
+            return new NpgsqlConnection(_companyContext._connection);
+        }
+
+        public async Task<IEnumerable<Employees>> Get()
+        {
+            var dataBase = dbConnection();
+            string sql = "SELECT * FROM employees WHERE status = '1'";
+            return await dataBase.QueryAsync<Employees>(sql, new { });
+        }
+
+        public async Task<bool> Save(Employees employee, int[] departments )
+        {
+            var dataBase = dbConnection();
+            string sql = "INSERT INTO employees (created_by, age, email, name, position, surname) VALUES (@Created_by, @Age, @Email, @Name, @Position, @Surname)";
+            var result = await dataBase.ExecuteAsync(sql, new
+            {
+                employee.Created_by,
+                employee.Age,
+                employee.Email,
+                employee.Name,
+                employee.Position,
+                employee.Surname
+            });
+
+            if (result > 0)
+            {
+                string sqlLastEmployee = @"SELECT ""id"" FROM employees ORDER BY ""id"" ASC offset ((select count(*) from employees)-1)";
+                var lastId = await dataBase.QueryAsync(sqlLastEmployee, new { });
+                foreach (var item in departments)
+                {
+                    string sqlDepartments = "INSERT INTO departments_employees (created_by, id_department, id_employee) VALUES ('admin', @Id_department, @Id_employee)";
+                    var result2 = await dataBase.ExecuteAsync(sqlDepartments, new
+                    {
+                        Id_department = item,
+                        id_employee = lastId
+                    });
+                }
+            }   
+            return result2 > 0;
+        }
+
+        public async Task<bool> Update(int id, Employees employee, int[] departments)
+        {
+            var dataBase = dbConnection();
+            string sql = @"UPDATE employees SET
+                            modified_by = @Modified_by,
+                            modified_date = @Modified_date,
+                            status = @Status,
+                            age = @Age,
+                            email = @Email,
+                            name = @Name,
+                            position = @Position,
+                            surname = @Surname
+                           WHERE id = @Id ";
+            var result = await dataBase.ExecuteAsync(sql, new 
+            { 
+                employee.Modified_by,
+                employee.Modified_date,
+                employee.Status,
+                employee.Age,
+                employee.Email,
+                employee.Name,
+                employee.Position,
+                employee.Surname,
+                Id = id
+            });
+            if (result > 0)
+            {
+                string deleteDepartments = @"DELETE FROM departments_employees WHERE id_employee = @Id";
+                var deleteResult = await dataBase.ExecuteAsync(deleteDepartments, new { Id = id});
+                if (deleteResult > 0)
+                {
+                    foreach (var item in departments)
+                    {
+                        string sqlDepartments = "INSERT INTO departments_employees (created_by, id_department, id_employee) VALUES ('admin', @Id_department, @Id_employee)";
+                        var result2 = await dataBase.ExecuteAsync(sqlDepartments, new
+                        {
+                            Id_department = item,
+                            id_employee = id
+                        });
+                    }
+                }
+            }
+            return result2 > 0;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var dataBase = dbConnection();
+            string sql = "UPDATE employees SET status = false WHERE id = @Id ";
+            var result = await dataBase.ExecuteAsync(sql, new { Id = id });
+            return result > 0;
+        }
+
+    }
+}
+
+public interface IEmployeesServices
+{
+    Task<IEnumerable<Employees>> Get();
+    Task<bool> Save(Employees employee, int[] departments);
+
+    Task<bool> Update(int id, Employees employee, int[] departments);
+
+    Task<bool> Delete(int id);
+}
